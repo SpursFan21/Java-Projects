3420b7cbd1303ad11be2fcc4ca1577fd868ad3a2
Duncan Turner <duncanfrederic@gmail.com>
2024-01-11 08:44:41 +1300

    Grape addition

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
         Apple apple = new Apple("Fruit Basket A", "Red Delicious");
         Banana banana = new Banana("Fruit Basket B", 8);
         Orange orange = new Orange("Fruit Basket C", true);
+        Grape grape = new Grape("Fruit Basket D", 10);
 
         // Display information about fruits
         apple.display();
         banana.display();
         orange.display();
+        grape.display();
     }
 }
