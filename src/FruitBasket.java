ef2dc7723670bb8be14f937cba4972020c342ce5
Duncan Turner <duncanfrederic@gmail.com>
2024-01-18 10:02:32 +1300

    n

--- src/FruitBasket.java
+++ src/FruitBasket.java
@@ -66,7 +66,7 @@ class Grape extends FruitBasket {
     @Override
     public void display(){
         super.display();
-        System.out.println("Sweetness Level: " + sweetnessLevel);
+        System.out.println("Sweet  ness Level: " + sweetnessLevel);
         System.out.println("The best");
     }
 }
